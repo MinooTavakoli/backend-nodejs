5a9e37e688222940a777db624ecb6d3a59c78c75
Minoo Tavakoli <minootavakoli28@mail.com>
2023-05-12 18:16:56 +0430

    used morgan middleware

--- src/introduction/body.js
+++ src/introduction/body.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
+// app.use(morgan("tiny"));
+// app.use(morgan("dev"));
+// app.use(morgan("combined"));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms ")
+);
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
 
 app.post("/body", (req, res) => {
   console.log(req.body);
